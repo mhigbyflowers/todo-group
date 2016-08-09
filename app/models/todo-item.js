4243b392d87aa177365be1374bd83c74378b66dc
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-09 12:20:17 -0500

    styles look sharp

--- app/models/todo-item.js
+++ app/models/todo-item.js
@@ -4,6 +4,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   done: DS.attr('boolean'),
-  title: DS.attr('string'),
   group: DS.belongsTo('todo-group'),
 });
--- app/todo-group/detail/controller.js
+++ app/todo-group/detail/controller.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    destroy(value){
+      value.destroyRecord();
+    },
+    editSubmit(group, formValues){
+
+      group.setProperties(formValues);
+      group.save().then(() => {
+
+      // Redirect
+      this.transitionToRoute('todo-group.index');
+    });
+  }
+}
 });
